f0872dd36f7894b7539a041154371d6ba5902e16
Pavel <rezusx@mail.ru>
2024-04-21 02:18:08 +0300

    fix

--- HW-SpringAnimation2/ViewController.swift
+++ HW-SpringAnimation2/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
 
     @IBOutlet var springView: SpringView!
     @IBOutlet var springLabel: SpringLabel!
+    @IBOutlet var springButton: SpringButton!
+    
+    override func viewWillLayoutSubviews() {
+        springView.layer.cornerRadius = 10
+        springButton.layer.cornerRadius = 10
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,15 +35,31 @@ class ViewController: UIViewController {
         
         sender.setTitle("Run \(param.1)", for: .normal)
         
-        springView.animation = param.0
-        springView.curve = param.1
-        springView.force = param.2
-        springView.duration = param.3
-        springView.delay = param.4
+        springView.animation = param.preset
+        springView.curve = param.curve
+        springView.force = param.force
+        springView.duration = param.duration
+        springView.delay = param.delay
+        springView.damping = param.damping
+        springView.velocity = param.velocity
+        springView.scaleX = param.scale
+        springView.scaleY = param.scale
+        springView.rotate = param.rotate
         springView.animate()
     }
     
-    func getRandomAnimate() -> (String, String, CGFloat, CGFloat, CGFloat) {
+    func getRandomAnimate() -> (
+        
+        preset: String,
+        curve: String,
+        force: CGFloat,
+        duration: CGFloat,
+        delay: CGFloat,
+        damping: CGFloat,
+        velocity: CGFloat,
+        scale: CGFloat,
+        rotate: CGFloat
+    ) {
         
         let allPresets = AnimationPreset.allCases
         let randomIndexPreset = Int.random(in: 0..<allPresets.count)
@@ -49,15 +71,33 @@ class ViewController: UIViewController {
         
         let curve = allCurves[randomIndexCurve]
         
-        let force = CGFloat.random(in: 0...2)
+        let force = CGFloat.random(in: 1...7)
         
-        let duration = CGFloat.random(in: 0...2)
+        let duration = CGFloat.random(in: 0.5...3)
         
-        let delay = CGFloat.random(in: 0...2)
+        let delay = CGFloat.random(in: 0.5...1)
         
-        springLabel.text = "preset: \(preset.rawValue)\ncurve: \(curve.rawValue)\nforce: \(String(format: "%.2f", force))\nduration: \(String(format: "%.2f", duration))\ndelay: \(String(format: "%.2f", delay)) "
+        let damping = CGFloat.random(in: 0...1)
+        
+        let velocity = CGFloat.random(in: 0...1)
+        
+        let scale = CGFloat.random(in: 0...5)
+        
+        let rotate  = CGFloat.random(in: -5...5)
+        
+        springLabel.text = "preset: \(preset.rawValue)\ncurve: \(curve.rawValue)\nforce: \(String(format: "%.2f", force))\nduration: \(String(format: "%.2f", duration))\ndelay: \(String(format: "%.2f", delay))\ndamping: \(String(format: "%.2f", damping))\nvelocity: \(String(format: "%.2f", velocity))\nscale: \(String(format: "%.2f", scale))\nrotate: \(String(format: "%.2f", rotate))  "
 
-        return (preset.rawValue, curve.rawValue, force, duration, delay)
+        return (
+            preset.rawValue,
+            curve.rawValue,
+            force,
+            duration,
+            delay,
+            damping,
+            velocity,
+            scale,
+            rotate
+        )
     }
 }
 
